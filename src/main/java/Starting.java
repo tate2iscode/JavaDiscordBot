3f0956d713daedd2ce38613e503c42db119024c4
kimdongyun <kimdongyun03@gmail.com>
2022-01-31 01:04:03 +0900

    token

--- src/main/java/Starting.java
+++ src/main/java/Starting.java
@@ -7,7 +7,7 @@ import javax.security.auth.login.LoginException;
 
 public class Starting {
     public static void main(String[] args) throws LoginException {
-        JDABuilder builder = JDABuilder.createDefault("NzY5NzU2NjkwNDg4MjI5ODk4.X5TpyA.Pgl1OeaEASmE6K39LKKd2GD6Zyk");
+        JDABuilder builder = JDABuilder.createDefault("");
         // Disable parts of the cache
         builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
         // Enable the bulk delete event
